52d94747ca10ef9b8f521581337f41123fb01fe3
JulianSC-01 <julian.r.salati@gmail.com>
2024-08-30 16:00:59 -0400

    Convert sample application to standalone - root components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,24 +1,28 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { SharedModule } from 'js-shared';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        SharedModule,
-        RouterTestingModule
-      ],
-      declarations: [
         AppComponent
       ],
-    }).compileComponents();
+      providers: [
+        provideRouter([])
+      ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SharedModule } from 'js-shared';
 
 @Component({
+  imports: [
+    RouterOutlet,
+    SharedModule
+  ],
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html'
 })
 export class AppComponent {
