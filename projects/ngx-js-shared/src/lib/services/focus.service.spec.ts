4a6993ef7fab1b6ec8656dae2dcf6ae32fa754c0
JulianSC-01 <julian.r.salati@gmail.com>
2026-08-23 12:17:23 -0400

    Add unit tests for FocusService

--- projects/ngx-js-shared/src/lib/services/focus.service.spec.ts
+++ projects/ngx-js-shared/src/lib/services/focus.service.spec.ts
@@ -1,16 +1,93 @@
 import { TestBed } from '@angular/core/testing';
-import { beforeEach, describe, expect, test } from "vitest";
+import { afterEach, beforeEach, describe, expect, test, vi } from "vitest";
 import { FocusService } from './focus.service';
 
 describe('FocusService', () => {
   let service: FocusService;
 
+  const mockFocus = vi.fn();
+  const mockHTMLElement = {
+    focus: mockFocus
+  } as unknown as HTMLElement;
+
+  const focusElementNames = [
+    'mainNavbarLink', 'mainHeader', 'errorHeader', 'successHeader'
+  ];
+
   beforeEach(() => {
+    vi.useFakeTimers();
+
     TestBed.configureTestingModule({});
     service = TestBed.inject(FocusService);
+
+    vi.spyOn(mockHTMLElement, 'focus');
+    vi.spyOn(document, 'querySelector').
+      mockReturnValue(mockHTMLElement);
+
+    vi.spyOn(service, 'focusElement');
   });
 
-  test('should be created', () => {
-    expect(service).toBeDefined();
+  afterEach(() => {
+    vi.useRealTimers();
+
+    // Resets all call tracking for the focus function so
+    // we can assert it cleanly in each test.
+    mockFocus.mockClear();
   });
+
+  test('should focus custom element', async () => {
+    service.focusElement('#custom');
+
+    await vi.advanceTimersByTimeAsync(100);
+
+    expect(service.focusElement).
+      toHaveBeenLastCalledWith('#custom');
+    expect(document.querySelector).
+      toHaveBeenLastCalledWith('#custom');
+    expect(mockHTMLElement.focus).
+      toHaveBeenCalledOnce();
+  });
+
+  test('should not focus custom element', async () => {
+    vi.spyOn(document, 'querySelector').
+      mockReturnValue(null);
+
+    service.focusElement('#custom');
+
+    await vi.advanceTimersByTimeAsync(100);
+
+    expect(service.focusElement).
+      toHaveBeenLastCalledWith('#custom');
+    expect(document.querySelector).
+      toHaveBeenLastCalledWith('#custom');
+    expect(mockHTMLElement.focus).
+      not.toHaveBeenCalled();
+  });
+
+  test.for(focusElementNames)(
+    'should focus predefined elements',
+    async (elementName) => {
+      switch (elementName) {
+        case 'mainNavbarLink':
+          service.focusNavbar();
+          break;
+        case 'mainHeader':
+          service.focusMainHeader();
+          break;
+        case 'errorHeader':
+          service.focusErrorHeader();
+          break;
+        case 'successHeader':
+          service.focusSuccessHeader();
+      }
+
+      await vi.advanceTimersByTimeAsync(100);
+
+      expect(service.focusElement).
+        toHaveBeenLastCalledWith(`#${elementName}`);
+      expect(document.querySelector).
+        toHaveBeenLastCalledWith(`#${elementName}`);
+      expect(mockHTMLElement.focus).
+        toHaveBeenCalledOnce();
+    });
 });
